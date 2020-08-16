38a2df9f9dac2c5714415b89a7ca1889661d9c6c
sanghoo <sanghoo96@naver.com>
2020-08-16 20:23:13 +0900

    이메일전송 기본기능 추가

--- src/main/java/com/mycom/toyLMS/member/controller/MemberController.java
+++ src/main/java/com/mycom/toyLMS/member/controller/MemberController.java
@@ -1,5 +1,13 @@
 package com.mycom.toyLMS.member.controller;
 
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -82,6 +90,49 @@ public class MemberController {
 
 		return "redirect:/member/login";
 	}
-
-
+	
+	// 메일전송
+	@RequestMapping("sendMail")
+	public void sendEmail(@RequestParam("email") String email) throws Exception{
+		
+	    System.out.println(email);   
+		
+	    
+		//메일 관련 정보 
+        String host = "smtp.naver.com"; 
+        final String username = "***"; 	//네이버 이메일 주소중 @ naver.com 앞주소만 작성
+        final String password = "***"; 			//네이버 이메일 비밀번호를 작성
+        int port=587;                      				//네이버 STMP 포트 번호
+        
+        //메일 내용 
+        String recipient = "fladb25@naver.com";			 //메일을 발송할 이메일 주소를 기재해 줍니다.
+        String subject = "테스트 이메일입니다.";   			 //메일 발송시 제목을 작성
+        String body = "인증번호: 12345"; 					//메일 발송시 내용 작성
+        
+        Properties props = System.getProperties();
+       
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", port);
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.ssl.enable", "false");	
+        props.put("mail.smtp.ssl.trust", host);
+        props.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+ 
+        Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() { 
+            String un=username;
+            String pw=password; 
+            protected PasswordAuthentication getPasswordAuthentication() { 
+                return new PasswordAuthentication(un, pw); 
+            } 
+        });
+        session.setDebug(true); //for debug 
+        
+        Message mimeMessage = new MimeMessage(session); 
+        mimeMessage.setFrom(new InternetAddress("***@naver.com")); 
+        mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient)); 
+        mimeMessage.setSubject(subject); 
+        mimeMessage.setText(body); 
+        Transport.send(mimeMessage);
+        
+    }
 }
